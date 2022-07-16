310892d5072b8cc8ab71a7ea7a2aa753c59ca8f6
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-07-16 08:26:43 +0000

    remove database uri from code

--- src/config/development.py
+++ src/config/development.py
@@ -4,7 +4,3 @@ from src.config.base import Config
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        return "sqlite:///" + os.path.join(self.BASE_DIR, "sqlite.db")
--- src/config/production.py
+++ src/config/production.py
@@ -1,11 +1,5 @@
 from src.config.base import Config
-import os
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
-    uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    # rest of connection code using the connection string `uri`
