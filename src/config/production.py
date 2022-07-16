25ae8c2c8abe6cf3bc528bee516c81e8c08dc8f3
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-07-16 08:23:17 +0000

    database settings to replace postgres to postgresql

--- src/config/production.py
+++ src/config/production.py
@@ -4,5 +4,8 @@ import os
 
 class ProductionConfig(Config):
     DEBUG = False
-
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    # rest of connection code using the connection string `uri`
