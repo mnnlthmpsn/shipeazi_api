f8a86cba26dec79d004207aa48395cf3882de013
mnnlthmpsn <mnnlthmpsn@outlook.com>
2022-09-03 15:58:48 +0000

    server setup complete

--- src/config/production.py
+++ src/config/production.py
@@ -3,3 +3,10 @@ from src.config.base import Config
 
 class ProductionConfig(Config):
     DEBUG = False
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self):
+        conString = "postgresql://shipeazi_user:shipeazi_pwd@161.35.54.129:5432/shipeazi_db"
+        return conString
+
+    
